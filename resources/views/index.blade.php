41c5b7a0f2ad816e70147f413afc8947debce671
Marko Babović <mb20170119@student.fon.bg.ac.rs>
2021-07-01 22:40:31 +0200

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Bus</title>
+
+    <style>
+   .btn{
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 100px;
+    }
+    h1{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+    }
+    </style>
+</head>
+<body>
+    
+    <h1>Welcome</h1>
+
+    <a class="btn btn-primary" href="http://127.0.0.1:8000/passenger">Passenger</a>
+    <a class="btn btn-primary" style="margin-top:3px" href="http://127.0.0.1:8000/schedule">Schedule</a>
+</body>
+</html>
